648a3a4e80f1bc9d47e902fc029d7b8f8cc3f505
M@W <mehdicr7wahid@gmail.com>
2023-08-17 12:56:41 +0430

    Design system config added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,52 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        primary: {
+          900: "#181D25",
+          800 : "#393E46",
+          700 : "#546378",
+          600 : "#7B889D",
+          500 : "#99A7BD",
+          400 : "#BFC9D9",
+          300 : "#E1E4EA",
+          200 : "#EAEAF1",
+          100 : "#F6F7F9"
+        },
+
+        myAccent : {
+          error : {
+            500 : "#8E1010",
+            400 : "#AE4137", 
+            300 : "#D62929", 
+            200 : "#ECB0AC", 
+            100 : "#FAD5D1"
+          },
+          warning: {
+            500 : "#917D08",
+            400 : "#BA9E12",
+            300 : "#F2D40D", 
+            200 : "#F9E686", 
+            100 : "#FCF5CF"
+          },
+          success: {
+            500 : "#076105",
+            400 : "#1C971C", 
+            300 : "#21D460", 
+            200 : "#86F9A3" ,
+            100 : "#CFFCD2"
+          },
+        }
+
+
       },
     },
   },
   plugins: [],
-}
+};
